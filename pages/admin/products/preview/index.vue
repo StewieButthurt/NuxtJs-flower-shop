0969e34f74a33dd29aece516d7814c6645182e9c
StewieButthurt <nosoff.slawa@yandex.ru>
2020-05-12 08:11:39 +0400

    Рефакторинг страницы preview. Перенос стилей в модуль swiper

--- pages/admin/products/preview/index.vue
+++ pages/admin/products/preview/index.vue
@@ -545,19 +545,6 @@
         +md-block
             justify-content: center
     
-    .preview__swiper
-        display: flex
-        flex-direction: column
-        align-items: center
-        max-width: 450px
-        margin-top: 15px
-        +size(5)
-        +lgX-block
-            margin-top: 2%
-        +size-md(6)
-        +size-xs(11)
-        box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)
-    
     .preview__descr
         +size(6)
         display: flex
@@ -643,78 +630,11 @@
         width: 161px
         height: 52px
     
-    .preview__swiper-image
-        position: relative
-        width: 100%
-        padding-top: 100%
-        transition: transform 0.2s
-        cursor: pointer
-    
-    
-    .preview__swiper-images-wrapper
-        display: flex
-        align-items: center
-        width: 100%
-        justify-content: center
-        margin-top: 5%
-        margin-bottom: 5%
-    
     .preview__swiper-image-item
         +size(2)
         margin-left: 1%
         margin-right: 1%
         cursor: pointer
-    
-    .preview__swiper-image-container
-        overflow: hidden
-        width: 100%
-        position: relative
-    
-    .preview__swiper-image-mouseenter
-        transform: scale(2)
-    
-    .preview__swiper-discount-price
-        width: 50px
-        height: 50px
-        color: white
-        display: flex
-        justify-content: center
-        position: absolute
-        align-items: center
-        font-size: 16px
-        background-color: rgba(255, 0, 0, 0.7)
-        border-radius: 100%
-        top: 3px
-        right: 3px
-        cursor: pointer
-    
-    .preview__swiper-info
-        display: flex
-        flex-direction: column
-        position: absolute
-        top: 0px
-        left: 0px
-        padding-left: 10px
-        padding-right: 10px
-        cursor: pointer
-    
-    .preview__swiper-info-stock
-        width: 130px
-        height: 22px
-        margin-top: 10px
-        color: white
-        display: flex
-        justify-content: center
-        align-items: center
-        text-transform: uppercase
-        background-color: rgba(255, 0, 0, 0.7)
-        font-size: 12px
-
-    .preview__swiper-info-bestseller
-        background-color: rgba(255, 168, 0, 0.7)
-    
-    .preview__swiper-info-week-price
-        background-color: rgba(0, 194, 255, 0.7)
     
     .preview__specifications
         +size(11)
