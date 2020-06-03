2b2915dc1e0f1d665ddefe9a54311352391ec785
StewieButthurt <nosoff.slawa@yandex.ru>
2020-06-03 16:40:18 +0400

    Добавлен автологин в хуке validate

--- pages/admin/products-edit/edit/preview/index.vue
+++ pages/admin/products-edit/edit/preview/index.vue
@@ -64,6 +64,19 @@
 
     export default {
         layout: 'admin',
+        async validate({ store, redirect, $axios }) {
+            try {
+                
+                if(!store.getters['modules/auth/token']) {
+                    await store.dispatch('modules/auth/autoLogin')
+                }
+
+                await $axios.$get('/api/auth/admin/token')
+                return true
+            } catch(e) {
+                redirect('/login?message=login')
+            }
+        },
         head: {
             title: 'Панель администратора | Предпросмотр'
         },
